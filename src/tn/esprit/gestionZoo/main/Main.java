bca47d8601eb5781f46cdd097f1fedb368ce5102
hajermabrouk <101023542+hajermabrouk@users.noreply.github.com>
2023-12-09 23:05:48 +0100

    prosit4

---
+++ src/tn/esprit/gestionZoo/main/Main.java
@@ -0,0 +1,43 @@
+package tn.esprit.gestionZoo.main;
+
+import tn.esprit.gestionZoo.entities.Animal;
+import tn.esprit.gestionZoo.entities.Zoo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Zoo myZoo1 = new Zoo("belvidère", "tunis"); //instruction 7
+        Zoo myZoo2 = new Zoo("zoo", "Ariana");
+        Zoo myZoo5 = new Zoo();
+        myZoo5.setName("");  // tester instructin 18
+        myZoo5.setCity("tunis");
+        myZoo1.displayZoo(); // instruction 8
+        System.out.println(myZoo1);
+        System.out.println(myZoo1.toString());
+        Animal lion = new Animal("chiens", "loulou", 1, true); // instruction 7
+        System.out.println(lion); // // instruction 9
+        System.out.println(lion.toString()); // instruction 9
+        Animal chat = new Animal("chats", "simba", 2, true);
+        Animal chien = new Animal("chiots", "foufou", 2, true);
+        Animal chat2 = new Animal("chiots", "simba2", 2, true);
+        Animal chien2 = new Animal("chiots", "foufou2", 3, true);
+        Animal chien3 = new Animal();//  tester instruction 18
+        chien3.setName("foufou3");
+        chien3.setFamily("chiots");
+        chien3.setAge(-2);  // tester instruction 18
+        chien3.setMammal(true);
+        System.out.println(myZoo1.addAnimal(lion));
+        System.out.println(myZoo2.addAnimal(chat));
+        myZoo1.afficherAnimal(); // appel instruction 11
+        System.out.println("l'indice est :"+ myZoo1.searchAnimal(chat)); //  instruction 11
+        System.out.println(myZoo1.addUniq(chien)); //instrurctin 12
+        System.out.println(myZoo1.addUniq(lion)); // instrurctin 12
+        System.out.println(myZoo1.addUniq(chat2));
+        System.out.println(myZoo1.addUniq(chien2));
+        System.out.println(myZoo1.removeAnimal(lion));
+        myZoo1.afficherAnimal();
+        System.out.println(myZoo1.isZooFull());
+        System.out.println("le zoo qui contient plus d'animaux :"+ Zoo.CamparerZoo(myZoo1,myZoo2));
+
+    }
+}
