c22c92250b8c5b266ecb75f35cbbf0050a979a5f
hajermabrouk <101023542+hajermabrouk@users.noreply.github.com>
2023-12-09 22:38:26 +0100

    prosit 3

--- Zoo.java
+++ Zoo.java
@@ -4,7 +4,7 @@ class Animal {
     String name;
     int age;
     boolean isMammal;
-     // instruction 6
+    // instruction 6
     public Animal(String family, String name, int age, boolean
             isMammal) {
         this.family = family;
@@ -24,43 +24,53 @@ public class Zoo {
     Animal[] animals;
     String name;
     String city;
-    int nbrCages;
+
+    final int nbrCages = 25; // instruction 14
 
     // instruction 6
-    public Zoo(String name, String city, int nbrCages) {
+    public Zoo() {
+    }
+
+    public Zoo(String name, String city) {
         animals = new Animal[nbrCages];
         this.name = name;
         this.city = city;
-        this.nbrCages = nbrCages;
     }
 
-    boolean addAnimal(Animal animal) {   // instruction 10
+    // instruction 10
+    boolean addAnimal(Animal animal) {
 
-     for (int i = 0; i < nbrCages; i++) {
-         if (animals[i] == null) {
-             animals[i] = animal;
-             return true;
-         }
-     }
-     return false;
- }
-    public void displayZoo() { // instruction 8
+        for (int i = 0; i < nbrCages; i++) {
+            if (animals[i] == null) {
+                animals[i] = animal;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // instruction 8
+    public void displayZoo() {
         System.out.println("name: " + this.name + " city: " + this.city + " nbrCages: " + this.nbrCages);
 
     }
 
-    public String toString() {  // instruction 9
+    // instruction 9
+    public String toString() {
         return name + " " + city + " " + nbrCages;
     }
+
     //instruction 11
-    public void  afficherAnimal() { // instruction 11
+    public void afficherAnimal() {
         for (int i = 0; i < nbrCages; i++) {
             if (animals[i] != null) {
                 System.out.println(animals[i]);
             }
         }
     }
-    public int searchAnimal(Animal animal) { // instruction 11
+
+    // instruction 11
+    public int searchAnimal(Animal animal) {
         for (int i = 0; i < nbrCages; i++) {
             if (animals[i] == animal) {
                 return i;
@@ -68,43 +78,83 @@ public class Zoo {
         }
         return -1;
     }
-    boolean addUniq(Animal animal) { //instruction 12
+
+    //instruction 12
+    boolean addUniq(Animal animal) {
 
         for (int i = 0; i < nbrCages; i++) {
-            if ((searchAnimal(animal)==-1)&&(animals[i]==null)) {
+            if ((searchAnimal(animal) == -1) && (animals[i] == null)) {
                 animals[i] = animal;
                 return true;
             }
         }
         return false;
     }
-    boolean removeAnimal(Animal animal){ // instruction 13
-   int indice=searchAnimal(animal);
-            if (indice!=-1){
-                animals[indice]=null;
-               return true; // suppression réuissie
-            }
+
+    // instruction 13
+    boolean removeAnimal(Animal animal) {
+        int indice = searchAnimal(animal);
+        if (indice != -1) {
+            animals[indice] = null;
+            return true; // suppression réuissie
+        }
         return false;
     }
+
+
+    // instruction 15
+    public int totalAnimals() {
+        int nbrAnimaux = 0;
+        for (Animal animal : animals) {
+            if (animal != null)
+                nbrAnimaux++;
+
+        }
+        return nbrAnimaux;
+    }
+
+    boolean isZooFull() {
+
+        if (totalAnimals() == nbrCages) {
+            System.out.println("le zoo est plein");
+            return true;
+        }
+        return false;
+    }
+
+    //instruction 16
+    public static Zoo CamparerZoo(Zoo z1, Zoo z2) {
+    if (z1.totalAnimals()>z2.totalAnimals())
+         return z1;
+    else if (z1.totalAnimals()<z2.totalAnimals())
+        return z2;
+    else return null;
+    }
+
     public static void main(String[] args) {
-        Zoo myZoo = new Zoo("belvidère", "tunis", 25); //instruction 7
-        myZoo.displayZoo(); // instruction 8
-        System.out.println(myZoo);
-        System.out.println(myZoo.toString());
+       Zoo myZoo1 = new Zoo("belvidère", "tunis"); //instruction 7
+        Zoo myZoo2 = new Zoo("zoo", "Ariana"); //instruction 7
+
+        myZoo1.displayZoo(); // instruction 8
+        System.out.println(myZoo1);
+        System.out.println(myZoo1.toString());
         Animal lion = new Animal("chiens", "loulou", 1, true); // instruction 7
         System.out.println(lion); // // instruction 9
         System.out.println(lion.toString()); // instruction 9
         Animal chat = new Animal("chats", "simba", 2, true); // instruction 7
         Animal chien = new Animal("chiots", "foufou", 2, true); // instruction 7
+        Animal chat2 = new Animal("chiots", "simba2", 2, true); // instruction 7
 
-        System.out.println(myZoo.addAnimal(lion));
-        System.out.println(myZoo.addAnimal(chat));
-        myZoo.afficherAnimal(); // appel instruction 11
-        System.out.println("l'indice est :"+ myZoo.searchAnimal(chat)); // appel instruction 11
-        System.out.println(myZoo.addUniq(chien)); //appel instrurctin 12
-        System.out.println(myZoo.addUniq(lion)); //appel instrurctin 12
-        System.out.println(myZoo.removeAnimal(lion));
-        myZoo.afficherAnimal();
+        System.out.println(myZoo1.addAnimal(lion));
+        System.out.println(myZoo2.addAnimal(chat));
+        myZoo1.afficherAnimal(); // appel instruction 11
+        System.out.println("l'indice est :"+ myZoo1.searchAnimal(chat)); // appel instruction 11
+        System.out.println(myZoo1.addUniq(chien)); //appel instrurctin 12
+        System.out.println(myZoo1.addUniq(lion)); //appel instrurctin 12
+        System.out.println(myZoo1.addUniq(chat2));
+        System.out.println(myZoo1.removeAnimal(lion));
+        myZoo1.afficherAnimal();
+        System.out.println(myZoo1.isZooFull());
+        System.out.println("le zoo qui contient plus d'animaux :"+ Zoo.CamparerZoo(myZoo1,myZoo2));
     }
 }
-
